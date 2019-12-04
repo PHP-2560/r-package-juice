9b996a67333be4b184ea75d73090fbc6a3ff8a69
camerinrencken <55039954+camerinrencken@users.noreply.github.com>
2019-12-04 13:49:16 -0500

    delete

--- Functions-Rencken.R
+++
@@ -1,5 +0,0 @@
-This is going to be our .R file with all of our packages functions
-
-library(devtools)
-getwd()
-MyData <- read.csv(file="stds-by-disease-county-year-sex.csv", header=TRUE, sep=",")
